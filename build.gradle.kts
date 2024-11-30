00291ad64b3a8205138945d7862339e38d591f41
anviaan <andre.carbajal.vargas@gmail.com>
2024-11-29 19:40:04 -0500

    Add task to print environment variables to a file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,4 +64,13 @@ subprojects {
     tasks.withType<GenerateModuleMetadata>().configureEach {
         enabled = false
     }
+
+    tasks.register("printEnv") {
+        doLast {
+            val envFile = File(System.getenv("GITHUB_ENV"))
+            envFile.appendText("MOD_VERSION=$MOD_VERSION\n")
+            envFile.appendText("RELEASE_NAME=$ARCHIVE_NAME-$MOD_VERSION\n")
+            envFile.appendText("GAME_VERSIONS=$COMPATIBLE_VERSIONS\n")
+        }
+    }
 }
